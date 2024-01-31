b62a45e47bbc9733a2c0a2752f8cff8187bc41f4
becom1ng <becom1ng@users.noreply.github.com>
2024-01-31 11:09:39 -0600

    Add spinner. Fix alt text.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
 	currentPage: window.location.pathname,
 };
 
+// Display top 20 movies by popularity
 async function displayPopularMovies() {
 	const { results } = await fetchAPIData('movie/popular');
 	console.log(results);
@@ -10,32 +11,33 @@ async function displayPopularMovies() {
 		const div = document.createElement('div');
 		div.classList.add('card');
 		div.innerHTML = `
-          <a href="movie-details.html?id=${movie.id}">
-            ${
-							movie.poster_path
-								? `<img
-                src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-                class="card-img-top"
-                alt="Movie Title"
-              />`
-								: `<img
-              src="images/no-image.jpg"
-              class="card-img-top"
-              alt="Movie Title"
+        <a href="movie-details.html?id=${movie.id}">
+        ${
+					movie.poster_path
+						? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
             />`
-						}
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">${movie.title}</h5>
-            <p class="card-text">
-              <small class="text-muted">Release: ${movie.release_date}</small>
-            </p>
-          </div>`;
+						: `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+            />`
+				}
+        </a>
+        <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+        </div>`;
 		document.querySelector('#popular-movies').appendChild(div);
 	});
 }
 
-async function displayPopularTVShows() {
+// Display top 20 tv shows by popularity
+async function displayPopularShows() {
 	const { results } = await fetchAPIData('tv/popular');
 	console.log(results);
 
@@ -49,24 +51,27 @@ async function displayPopularTVShows() {
 								? `<img
                 src="https://image.tmdb.org/t/p/w500${show.poster_path}"
                 class="card-img-top"
-                alt="Show Title"
+                alt="${show.name}"
               />`
 								: `<img
               src="images/no-image.jpg"
               class="card-img-top"
-              alt="Show Title"
+              alt="${show.name}"
             />`
 						}
           </a>
           <div class="card-body">
             <h5 class="card-title">${show.name}</h5>
             <p class="card-text">
-              <small class="text-muted">Release: ${show.first_air_date}</small>
+              <small class="text-muted">First Air Date: ${
+								show.first_air_date
+							}</small>
             </p>
           </div>`;
 		document.querySelector('#popular-shows').appendChild(div);
 	});
 }
+
 // Fetch data from API
 async function fetchAPIData(endpoint) {
 	// Only use this for development or very small projects.
@@ -74,12 +79,23 @@ async function fetchAPIData(endpoint) {
 	const API_KEY = '30c944644c462584bb8878ea91643e2c';
 	const API_URL = 'https://api.themoviedb.org/3/';
 
+	showSpinner();
+
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
 	);
 	const data = await response.json();
 
+	hideSpinner();
+
 	return data;
+}
+
+function showSpinner() {
+	document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+	document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -100,7 +116,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			displayPopularTVShows();
+			displayPopularShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie Details');
