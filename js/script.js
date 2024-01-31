e5ab103c1685716e07cbedfd7e4f56fd647b7954
becom1ng <becom1ng@users.noreply.github.com>
2024-01-31 10:57:33 -0600

    Added popular tv shows.

--- js/script.js
+++ js/script.js
@@ -35,8 +35,42 @@ async function displayPopularMovies() {
 	});
 }
 
+async function displayPopularTVShows() {
+	const { results } = await fetchAPIData('tv/popular');
+	console.log(results);
+
+	results.forEach((show) => {
+		const div = document.createElement('div');
+		div.classList.add('card');
+		div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+							show.poster_path
+								? `<img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="Show Title"
+              />`
+								: `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Show Title"
+            />`
+						}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${show.first_air_date}</small>
+            </p>
+          </div>`;
+		document.querySelector('#popular-shows').appendChild(div);
+	});
+}
 // Fetch data from API
 async function fetchAPIData(endpoint) {
+	// Only use this for development or very small projects.
+	// Key should be stored and used from a server.
 	const API_KEY = '30c944644c462584bb8878ea91643e2c';
 	const API_URL = 'https://api.themoviedb.org/3/';
 
@@ -66,7 +100,7 @@ function init() {
 			displayPopularMovies();
 			break;
 		case '/shows.html':
-			console.log('Shows');
+			displayPopularTVShows();
 			break;
 		case '/movie-details.html':
 			console.log('Movie Details');
