43d2c9ae9657db79352d25e61805627cda17b431
becom1ng <becom1ng@users.noreply.github.com>
2024-01-31 12:19:55 -0600

    Generate backdrop for detail pages. Implement for movies.

--- js/script.js
+++ js/script.js
@@ -77,6 +77,9 @@ async function displayMovieDetails() {
 	const movieId = window.location.search.split('=')[1];
 	const movie = await fetchAPIData(`movie/${movieId}`);
 
+	// Background image overlay
+	displayBackgroundImage('movie', movie.backdrop_path);
+
 	const div = document.createElement('div');
 	div.innerHTML = `<div class="details-top">
     <div>
@@ -133,6 +136,28 @@ class="card-img-top" alt="${movie.title}"/>`
 	document.querySelector('#movie-details').appendChild(div);
 }
 
+//  Display background on details pages
+function displayBackgroundImage(type, backgroundPath) {
+	const overlayDiv = document.createElement('div');
+	overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backgroundPath})`;
+	overlayDiv.style.backgroundSize = 'cover';
+	overlayDiv.style.backgroundPosition = 'center';
+	overlayDiv.style.backgroundRepeat = 'no-repeat';
+	overlayDiv.style.height = '100vh';
+	overlayDiv.style.width = '100vw';
+	overlayDiv.style.position = 'absolute';
+	overlayDiv.style.top = '0';
+	overlayDiv.style.left = '0';
+	overlayDiv.style.zIndex = '-1';
+	overlayDiv.style.opacity = '0.1';
+
+	if (type === 'movie') {
+		document.querySelector('#movie-details').appendChild(overlayDiv);
+	} else {
+		document.querySelector('#show-details').appendChild(overlayDiv);
+	}
+}
+
 // Fetch data from API
 async function fetchAPIData(endpoint) {
 	// Only use this for development or very small projects.
@@ -145,6 +170,7 @@ async function fetchAPIData(endpoint) {
 	const response = await fetch(
 		`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
 	);
+
 	const data = await response.json();
 
 	hideSpinner();
